dde23aa1333c3f739651a240f0e4534a3155b35d
tuananh <hvtuananh@gmail.com>
2014-12-12 15:16:26 -0500

    fixed check alive script

--- check_alive.sh
+++ check_alive.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source $HOME/.bash_profile
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ps caux | grep java | grep twitter | grep tuananh &> /dev/null
+ps aux | grep java | grep twitter | grep tuananh &> /dev/null
 if [ $? -ne 0 ]; then
     echo "No crawler is running!"
     pushd $DIR &> /dev/null
